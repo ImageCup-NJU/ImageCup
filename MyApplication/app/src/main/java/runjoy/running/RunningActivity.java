2be40225250840c3348bffc47212954db0958098
Carter1996 <wangjc19960926@163.com>
2017-04-03 18:33:35 +0800

    修正

--- MyApplication/app/src/main/java/runjoy/running/RunningActivity.java
+++ MyApplication/app/src/main/java/runjoy/running/RunningActivity.java
@@ -286,6 +286,7 @@ public class RunningActivity extends AppCompatActivity implements LocationListen
                     mlocMarker.showInfoWindow();
                 }
                 presenter.startMonitor(NumberUtils.doubleStander(((double) mDistance) / 1000.0));
+                System.out.println((((double) mDistance) / 1000.0) + "xxxxx");
             }
         } else if (lineID == 2) {
             if (linepoints != null && linepoints.size() > 0) {
